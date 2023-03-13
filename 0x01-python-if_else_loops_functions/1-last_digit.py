d580375ff45707b27e30b4a446731e4be6c8f653
edinabwari <bwarikwambokaedina@gmail.com>
2023-03-13 22:29:11 +0300

    intergers in python do not support the indexing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number[-1] == 0:
+if str(number)[-1] == 0:
     print(f"Last digit of {number} is {number[-1]} and is 0")
-elif number[-1] > 5:
+elif str(number)[-1] > 5:
     print(f"Last digit of {number} is {number[-1]} and is greator than 5")
 else:
     print(f"Last digit of{number} is {number[-1]} and is less than 6 and not 0")
