9c9bb3e2d0f1297ba19851be6a27cd5f79808488
edinabwari <bwarikwambokaedina@gmail.com>
2023-03-31 15:55:34 +0300

    not yet done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__import__":
-    
+    from sys import argv
+    num = len(argv) - 1
+    print("{:d} {:s}{:s}".formart(num,
+                                  "argument" if num <=2 and num == 1
+                                  else "arguments",
+                                  "." if num == 0 else ":"))
+    for a, s in enumerate(argv):
+        if a > 0:
+            print("{}: {}".format(a, s))
